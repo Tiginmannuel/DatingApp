6483d15767d5dbac61ea180f84a712f4bcfa2295
Tigin Mannuel <tiginmannuel324@gmail.com>
2021-11-29 02:35:19 +0530

    DatingApp First Commit

---
+++ DatingApp/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using DatingApp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Data
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions options) : base(options)
+		{
+
+		}
+
+		public DbSet<AppUser> Users { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<AppUser>(u =>
+			{
+				u.HasKey(e => new { e.UserId });
+			});
+		}
+	}
+}
